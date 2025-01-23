ed06bfe1fb456e79dbc46427ce6bcd8336f799da
leVert <selmamathis15@gmail.com>
2025-01-23 14:43:45 +0300

    updated route

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
-@app.get("/api/auth-fastapi") 
+@app.get("/api/auth-fastapi/") 
 async def test():
     return {"message": "Hello World"}
