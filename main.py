13df9fcb51b824fc858fbf66a80105d27b3017d0
leVert <selmamathis15@gmail.com>
2025-01-23 10:21:45 +0300

    added requirement files

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+app = FastAPI()
+
+@app.get("/test/") 
+async def test():
+    return {"message": "Hello World"}
