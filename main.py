49334c37217f24fcbbc8d7988a95e073991e534d
leVert <selmamathis15@gmail.com>
2025-01-24 11:00:30 +0300

    db connection test

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 from routes.signup import router as signup_router
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+
+MONGO_URL = os.getenv("MONGO_URL")
+JWT_KEY = os.getenv("JWT_KEY")
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.get_default_database()
+
+def get_database():
+    return db
 
 app = FastAPI()
 
 router = APIRouter()
+@app.on_event("startup")
+async def startup_db_client():
+    try:
+        # Attempt to connect to the database
+        await client.server_info()
+        print("Connected to the MongoDB database!")
+    except Exception as e:
+        print(f"Failed to connect to the MongoDB database: {e}")
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    client.close()
 
 @router.get("/auth-fastapi/")
 async def read_root():
